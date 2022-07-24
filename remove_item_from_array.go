2601e02d7f5319ea3fe5e7c58662ea23f7ca9c5c
Alexa <64697981+quattroformaggi@users.noreply.github.com>
2022-07-24 18:21:20 +0300

    added problem statement

--- remove_item_from_array.go
+++ remove_item_from_array.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+/*Go does not have a built-in function to remove an element from a slice.
+
+Implement the Remove(nums []int, i int) []int function, which removes the element at index i from the nums slice.
+If a non-existent index arrives, then the original slice is returned from the function.
+
+The order of elements can change after an element is removed.*/
+
 func main() {
 	a := []int{1, 2, 3, 4, 5}
 	fmt.Println(Remove(a, 3))
